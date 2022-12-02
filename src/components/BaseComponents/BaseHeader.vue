b6f5a5371632392871f1995ac5828f345b0bbe39
DESKTOP-I7AKHO4\User <baotv11@fpt.com.vn>
2022-12-02 15:26:32 +0700

    ✨ feat(Base): Add Header

---
+++ src/components/BaseComponents/BaseHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="tabs">
+    <ul>
+      <li>Home</li>
+      <li>About Us</li>
+      <li>Services</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
+    <div>
+      <base-header></base-header>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import BaseHeader from "@/components/BaseComponents/BaseHeader.vue";
 @Options({
   components: {
     HelloWorld,
+    BaseHeader,
   },
 })
 export default class HomeView extends Vue {}
